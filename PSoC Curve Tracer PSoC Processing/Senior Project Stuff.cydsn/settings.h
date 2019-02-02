53ae2f8a5b5b0fd3bf0fa4b5318f642825ae20d1
thomaj3 <thomaj23@tcnj.edu>
2019-02-02 14:30:39 -0500

    Revert "optimized search process, no more duplicate Vds arrays, created a test function for a single point rather than one whole"
    This reverts commit 747243d6832611c9e19dfa424bf9963aa3618a9a.

--- PSoC Curve Tracer PSoC Processing/Senior Project Stuff.cydsn/settings.h
+++ PSoC Curve Tracer PSoC Processing/Senior Project Stuff.cydsn/settings.h
@@ -13,10 +13,12 @@
 #define VD_STEP_PMOS  1
 #define VC_STEP_NPN   1
 #define VC_SEPT_PNP   1
-//choose a for how many steps will be 255/VD_step + 1 steps
+//choose a for how many steps will be 255/VD_step steps
 //must be divisble of 255 i.e. 1,3,5,15,17,51,85,255
-#define CURVE_NUM     6        //Number of curves to plot
-#define ID_MAX        2000     //Max allowable current. Enter 100-fold the desired current in mA
+#define CURVE_NUM     6         //Number of curves to plot
+#define ID_MAX        2000      //Max allowable current. Enter 100-fold the desired current in mA
 #define ID_TH         50       //Current to define on state at 1 V Vds. Enter 100-fold the desired current in mA
+
+
 
 /* [] END OF FILE */
